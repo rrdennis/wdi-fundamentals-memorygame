8c6ef130dc5d7f6192a14a1e46e007e983f66e4b
rrdennis <rrdennis@gmail.com>
2019-08-09 17:25:54 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,28 @@
 console.log('Up and running!');
 
-// console.log('User flipped ' + cardOne);
-// console.log('User flipped ' + cardFour);
-
 var cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log('User flipped ' + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match!');
-  } else {
-    alert('Sorry, try again.');
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log('You found a match!');
+    } else {
+      console.log('Sorry, try again.');
+    }
+    cardsInPlay = [];
   }
 }
+
+function flipCard(cardId) {
+  let currentCard = cards[cardId];
+  console.log('User flipped ' + currentCard);
+  cardsInPlay.push(currentCard);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+flipCard(1);
+flipCard(3);
